caa1cdb83aa22b6be62444bafa810153c2fcce16
synthaxme <emanuelvidalrm@gmail.com>
2022-08-25 16:59:02 -0300

    added response iteration

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
+//Fetch options, we'll look into it later, not super importat right now
 var requestOptions = {
     method: 'GET',
     redirect: 'follow'
 };
-
-    async function foo() {
+//Asynchronous function to fech api data and returning it as object(as a json, with .json).
+async function foo() {
     let obj;
-    let firstName2;
+    let x;
+    let MatchC = document.getElementById("exemplo1").value;
     const res = await fetch('https://thronesapi.com/api/v2/Characters')
     obj = await res.json();
-    console.log(obj[0].firstName)
-    // firstName2 =  document.getElementById("userInput").value;
-    document.write("LAST NAME IS: " + obj[0].lastName);
+    x = document.getElementById("main")
+    // x.innerHTML = x.innerHTML + "<br>" + ("LAST NAME IS: " + obj[0].lastName);
+    console.log(obj)
+    //For loop to iterate through every object of the API response, and matching the firstName with the response.
+    //Later should be changed for a better algorithm? or it doesnt matter given the size of the length of the response (54 objects)
+    for (let i = 0; i < obj.length; i++){
+        if (obj[i].firstName === MatchC){
+            console.log(obj[i].lastName);
+            break;
+        }
+        else {
+            console.log("Coundn't find any character with that name");
+        }
     }
-
-    foo();
+}
