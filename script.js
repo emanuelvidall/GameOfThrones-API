47bcf5b4238780ad860f6d2e013762d6ec089c91
synthaxme <emanuelvidalrm@gmail.com>
2022-08-25 16:05:29 -0300

    Testing API on different html, fixed fetch

--- script.js
+++ script.js
@@ -3,40 +3,14 @@ var requestOptions = {
     redirect: 'follow'
 };
 
-    // const p = document.getElementById("myPelement")
-// fetch('https://thronesapi.com/api/v2/Characters')
-//     .then((response) => {
-//         const obj = JSON.parse(response);
-//         document.getElementById("myPelement").innerHTML = obj.firstName;
-//         return response.json();
-//     })
-    // .then((data) => {
-    //     p.innerText = data
-    // });
-
-    // const obj = JSON.parse(data);
-
-    // document.getElementById("myPelement").innerHTML = obj.firstName;
-
-
-// async function getAllCharacters() {
-//     const response = await fetch('https://thronesapi.com/api/v2/Characters');
-//     const data = await response.json();
-//     console.log('Data', data)
-//     return data;
-//     }
-
     async function foo() {
     let obj;
+    let firstName2;
     const res = await fetch('https://thronesapi.com/api/v2/Characters')
     obj = await res.json();
     console.log(obj[0].firstName)
+    // firstName2 =  document.getElementById("userInput").value;
+    document.write("LAST NAME IS: " + obj[0].lastName);
     }
 
     foo();
-
-    iMp = document.getElementById("name").value;
-
-    function butones(){
-        console.log(iMp);
-    }
